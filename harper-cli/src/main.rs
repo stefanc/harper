ba3f22dd7e088708546b57212ec0e7485d5254f3
Stefan Cosma <stefan@adima.ro>
2025-10-27 21:20:59 +0100

    feat(asciidoc): integrate AsciiDoc parser

--- harper-cli/src/main.rs
+++ harper-cli/src/main.rs
@@ -13,6 +13,7 @@ use anyhow::anyhow;
 use ariadne::{Color, Label, Report, ReportKind, Source};
 use clap::Parser;
 use dirs::{config_dir, data_local_dir};
+use harper_asciidoc::AsciiDocParser;
 use harper_comments::CommentParser;
 use harper_core::linting::{LintGroup, Linter};
 use harper_core::parsers::{Markdown, MarkdownOptions, OrgMode, PlainEnglish};
@@ -828,6 +829,7 @@ fn load_file(
         Some("org") => Box::new(OrgMode),
         Some("typ") => Box::new(harper_typst::Typst),
         Some("py") | Some("pyi") => Box::new(PythonParser::default()),
+        Some("adoc") | Some("asciidoc") => Box::new(AsciiDocParser::default()),
         _ => {
             if let Some(comment_parser) = CommentParser::new_from_filename(file, markdown_options) {
                 Box::new(comment_parser)
